8fc65d5c702ed3836d411631a7acfc339930c268
RJET-IOS开发04 <zhanglinsong@zhanglinsongdeMacBook-Pro.local>
2022-12-12 14:44:57 +0800

    .

---
+++ app/src/main/java/com/czy/app/bis/discovery/DiscoveryController.java
@@ -0,0 +1,9 @@
+package com.czy.app.bis.discovery;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/discovery")
+public class DiscoveryController {
+}
